62f985f7444426086755090220a0e5ed6b711714
Jacob Staehle <jacob@staehle.us>
2020-05-07 17:29:05 -0500

    better dev

--- README.md
+++ README.md
@@ -2,9 +2,10 @@
 
 ## Requirements
 
-2 usb drives:
-1 of the drives to be used to flash the ubuntu installer ISO to.  Do that.
-The other drive will be the drive you want to install on.
+  * 2 usb drives
+    * 1 of the drives to be used to flash the ubuntu installer ISO to.  Do that.
+	* The other drive will be the drive you want to install on.
+
 
 ## Start
 
@@ -16,13 +17,17 @@ Open the Terminal.
 
 Plug in the other usb.
 
-`dmesg | tail`
+```
+dmesg | tail
+```
 
 Make note of the 'sdX' device that is reported here.  This is your target usb drive.
 
 Enter a superuser terminal with:
 
-`sudo -i`
+```
+sudo -i
+````
 
 Then run these commands to install some req'd utilities:
 
@@ -33,14 +38,19 @@ apt install -y git
 
 Then download this repo:
 
-`git clone https://github.com/staehle/usbos.git`
+```
+git clone https://github.com/staehle/usbos.git
+```
 
-Then cd into there and source our var file, giving 1 argument of our target usb device (replace `sdX` with what you noted earlier):
+Then cd into there and source our var file, which will give us a safe-bash function to do so. Give that 1 argument of our target usb device (replace `sdX` with what you noted earlier):
 
 ```
 cd usbos
-source set-dev.sh /dev/sdX
+source set-dev.sh
+set-inst-dev /dev/sdb
 ```
+
+
 
 
 
--- set-dev.sh
+++ set-dev.sh
@@ -1,25 +1,66 @@
 #!/bin/bash
 
+_usbos_path=$(realpath $(dirname "${BASH_SOURCE[0]}"))
+CNORMAL="\033[1;0m"
+CSTRONG="\033[1;1m"
+CRED="\033[1;31m"
+CGREEN="\033[1;32m"
+CYELLOW="\033[1;33m"
+CPURPLE="\033[1;35m"
+CCYAN="\033[1;36m"
+
+function _echo_title() {
+	echo
+	echo -e "$(date +%T)| ${CSTRONG}*** ${CYELLOW}$@${CNORMAL} ***"
+}
+
+function _echo_subtitle() {
+	echo
+	echo -e "$(date +%T)| ${CSTRONG}   > ${CYELLOW}$@${CNORMAL} ***"
+}
+
+function _echo_info() {
+	echo
+	echo -e "$(date +%T)| ${CSTRONG} ~ ${CCYAN}$@${CNORMAL} ~"
+}
+
+function _echo_err() {
+	echo
+    echo -e "$(date +%T)| ${CSTRONG}${CRED}ERROR:${CNORMAL} ${CSTRONG}$@${CNORMAL}"
+}
+function _echo_confirm() {
+	echo
+    echo -e "$(date +%T)| ${CSTRONG}>>> ${CGREEN}$@${CNORMAL} <<< <--"
+}
+
 function set-inst-dev() {
 	local arg1="first arg must be usb install device (e.g. /dev/sdb)"
 	if [ -z "$1" ]; then
-		echo "usage: $arg1"
-		echo "error: missing"
+		_echo_info "usage: $arg1"
+		_echo_err "missing arg"
 		return
 	elif [ ! -e $1 ]; then
-		echo "usage: $arg1"
-		echo "error: does not exist"
+		_echo_info "usage: $arg1"
+		_echo_err "does not exist!"
 		return
 	fi
 	if [ $(id -u) -ne 0 ]; then
-		echo "error: run this script as root!"
+		_echo_err "you missed the 'sudo -i' step in the README.md!"
 		return
 	fi
-
 	export DEV="$1"
 	export DM="${DEV##*/}"
 
+	_echo_title "Your selected device:"
 	echo "Device: ${DEV}"
 	echo "    DM: ${DM}"
+
+	_echo_info "Please confirm this is the correct device:"
+
+	fdisk -l ${DEV}
+
+	_echo_confirm "If this is correct, continue with the next steps. If not, run this function again with the correct drive next time :)"
 }
 
+
+#[[ "$1" == "_test" ]] && shift && set-inst-dev $@
