ae3e7d74a87dc73292ec69e3b3770ce6914df3ca
Jacob Staehle <jacob@staehle.us>
2020-05-07 17:14:13 -0500

    update

--- set-dev.sh
+++ set-dev.sh
@@ -1,24 +1,25 @@
 #!/bin/bash
-arg1="first arg must be usb install device (e.g. /dev/sdb)"
 
-if [ -z "$1" ]; then
-	echo "usage: $arg1"
-	echo "error: missing"
-	exit 1
-elif [ ! -e $1 ]; then
-	echo "usage: $arg1"
-	echo "error: does not exist"
-	exit 1
-fi
+function set-inst-dev() {
+	local arg1="first arg must be usb install device (e.g. /dev/sdb)"
+	if [ -z "$1" ]; then
+		echo "usage: $arg1"
+		echo "error: missing"
+		return
+	elif [ ! -e $1 ]; then
+		echo "usage: $arg1"
+		echo "error: does not exist"
+		return
+	fi
+	if [ $(id -u) -ne 0 ]; then
+		echo "error: run this script as root!"
+		return
+	fi
 
-if [ $(id -u) -ne 0 ]; then
-	echo "error: run this script as root!"
-	exit 1
-fi
+	export DEV="$1"
+	export DM="${DEV##*/}"
 
-export DEV="$1"
-export DM="${DEV##*/}"
-
-echo "Device: ${DEV}"
-echo "    DM: ${DM}"
+	echo "Device: ${DEV}"
+	echo "    DM: ${DM}"
+}
 
